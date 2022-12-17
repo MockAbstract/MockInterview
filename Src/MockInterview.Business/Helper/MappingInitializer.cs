35738a7cd1fd0b1516288de60cc14d0761968f57
Nodirkhan Abdumurotov <nodirxonabdumurotov@mail.ru>
2022-12-17 23:25:07 +0500

    Add AutoMapper settings

---
+++ Src/MockInterview.Business/Helper/MappingInitializer.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using MockInterview.Domain.Entities;
+using MockInterview.Domain.Models.EmployeeDTO;
+
+namespace MockInterview.Business.Helper
+{
+    public class MappingInitializer : Profile
+    {
+        public MappingInitializer()
+        {
+            CreateMap<Employee, EmployeeDTO>().ReverseMap();
+        }
+    }
+}
