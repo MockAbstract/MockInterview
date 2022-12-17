0a896695c0bd133404e42bb99e8b9652300f697b
Nuriddin <isoqov0910@gmail.com>
2022-12-17 20:11:15 +0500

    Add data annatations to Category

--- Src/MockInterview.Domain/Entities/Category.cs
+++ Src/MockInterview.Domain/Entities/Category.cs
@@ -1,10 +1,19 @@
-﻿namespace MockInterview.Domain.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MockInterview.Domain.Entities
 {
     public class Category : BaseEntity
     {
+        [Required]
         public Guid Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        [Required]
         public string Description { get; set; }
+
+        [Required]
         public virtual ICollection<Employee> Specialist { get; set; }
     }
 }
