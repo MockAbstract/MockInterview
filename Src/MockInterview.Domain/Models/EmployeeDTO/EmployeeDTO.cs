f6a0ad8df3fcdeb91919b7266c209319f8556d28
Nodirkhan Abdumurotov <nodirxonabdumurotov@mail.ru>
2022-12-17 23:20:57 +0500

    Add employee Dto

--- Src/MockInterview.Domain/Models/EmployeeDTO/EmployeeDTO.cs
+++ Src/MockInterview.Domain/Models/EmployeeDTO/EmployeeDTO.cs
@@ -1,6 +1,53 @@
-﻿namespace MockInterview.Domain.Models.EmployeeDTO
+﻿using MockInterview.Domain.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace MockInterview.Domain.Models.EmployeeDTO
 {
     public class EmployeeDTO
     {
+        [Required]
+        public virtual Guid Id { get; set; }
+
+        [Required]
+        public virtual string FirstName { get; set; }
+
+        [Required]
+        public virtual string LastName { get; set; }
+
+        [Required]
+        public virtual Level Level { get; set; }
+
+        [Required]
+        public virtual string PhoneNumber { get; set; }
+
+        public virtual TimeSpan Experience { get; set; }
+
+        public virtual string ImagePath { get; set; }
+
+        [Required]
+        public virtual string Login { get; set; }
+
+        [Required]
+        [MinLength(5)]
+        [MaxLength(20)]
+        public virtual string Password { get; set; }
+
+        [Compare("Password")]
+        public virtual string ConfirmPassword { get; set; }
+
+        [Required]
+        public virtual Role Role { get; set; }
+
+        [Required]
+        public virtual string[] Permission { get; set; }
+
+        public virtual Guid CreatedBy { get; set; }
+
+        public virtual Guid UpdatedBy { get; set; }
+
+        public virtual DateTimeOffset CreatedDate { get; set; }
+
+        public virtual DateTimeOffset LastModifiedDate { get; set; }
+
     }
 }
---
+++ Src/MockInterview.Domain/Models/EmployeeDTO/EmployeeForCreationDTO.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MockInterview.Domain.Models.EmployeeDTO
+{
+    public class EmployeeForCreationDTO : EmployeeDTO
+    {
+        [JsonIgnore]
+        public override Guid Id { get; set; }
+
+        [JsonIgnore]
+        public override Guid CreatedBy { get; set; }
+
+        [JsonIgnore]
+        public override Guid UpdatedBy { get; set; }
+
+        [JsonIgnore]
+        public override DateTimeOffset CreatedDate { get; set; }
+
+        [JsonIgnore]
+        public override DateTimeOffset LastModifiedDate { get; set; }
+    }
+}
