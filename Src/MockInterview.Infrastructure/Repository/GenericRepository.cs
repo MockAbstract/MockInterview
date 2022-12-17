e07ebbf31be676e6bd068f35b0e0a2bf97bf4e64
Nodirkhan Abdumurotov <nodirxonabdumurotov@mail.ru>
2022-12-17 21:49:14 +0500

    Add GenerciRepository class

---
+++ Src/MockInterview.Infrastructure/Repository/GenericRepository.cs
@@ -0,0 +1,44 @@
+﻿using MockInterview.Domain;
+using MockInterview.Infrastructure.Interface;
+using System.Linq.Expressions;
+
+namespace MockInterview.Infrastructure.Repository
+{
+    public class GenericRepository<T> : IGenericRepositoryAsync<T> where T : class
+    {
+        public Task<T> CreateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> Find(Expression<Func<T, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> Find(Expression<Func<T, bool>> expression, List<string> tables)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
