f40c8946e5d3b9b59c1fefcb1e4d173070cdc27a
Nuriddin <isoqov0910@gmail.com>
2022-12-17 20:11:36 +0500

    Add data annotations to Interview

--- Src/MockInterview.Domain/Entities/Interview.cs
+++ Src/MockInterview.Domain/Entities/Interview.cs
@@ -1,19 +1,35 @@
 ﻿using MockInterview.Domain.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace MockInterview.Domain.Entities
 {
     public class Interview : BaseEntity
     {
+        [Key]
         public Guid Id { get; set; }
+ 
+        [Required]
         public Guid ClientId { get; set; }
         public Client Client { get; set; }
+
+        [Required]
         public Guid CategoryId { get; set; }
         public Category Category { get; set; }
+       
+        [Required]
         public Guid EployeId { get; set; }
         public Employee Employee { get; set; }
+
+        [Required]
         public DateTimeOffset InterviewDate { get; set; }
+        
+        [Required]
         public Level Level { get; set; }
+
+        [Required]
         public decimal Price { get; set; }
+        
+        [Required]
         public decimal PaymentStatus { get; set; }
     }
 }
