ff59cbead2638d5633427771bc31f869c2c14715
Nodirkhan Abdumurotov <nodirxonabdumurotov@mail.ru>
2022-12-18 02:57:36 +0500

    Create employee Controller

---
+++ Src/MockInterview.API/Controllers/EmployeeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MockInterview.Business.Interface;
+using MockInterview.Domain.Models.EmployeeDTO;
+
+namespace MockInterview.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeServiceAsync employeeServiceAsync;
+
+        public EmployeeController(IEmployeeServiceAsync employeeServiceAsync)
+        {
+            this.employeeServiceAsync = employeeServiceAsync;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> LoginAsync(LoginModel model)
+        {
+            var response = await this.employeeServiceAsync
+                .LoginAsync(model);
+
+            return StatusCode(response.StatusCode, response);
+        }
+    }
+}
