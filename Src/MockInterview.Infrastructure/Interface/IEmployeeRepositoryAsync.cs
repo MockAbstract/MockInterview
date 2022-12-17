20ed9f3d3dead995703e7bdcf7b4c9bc5d5a6101
Nodirkhan Abdumurotov <nodirxonabdumurotov@mail.ru>
2022-12-17 22:14:08 +0500

    Create interface IEmployeeRepositoryAsync

---
+++ Src/MockInterview.Infrastructure/Interface/IEmployeeRepositoryAsync.cs
@@ -0,0 +1,9 @@
+﻿using MockInterview.Domain.Entities;
+
+namespace MockInterview.Infrastructure.Interface
+{
+    public interface IEmployeeRepositoryAsync : IGenericRepositoryAsync<Employee>
+    {
+        public Task<Employee> LoginAsync(string login, string password);
+    }
+}
