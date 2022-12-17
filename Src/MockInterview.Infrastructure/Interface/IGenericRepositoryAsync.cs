d7f93b177d1dbb065495af072439358d3e05428f
Nodirkhan Abdumurotov <nodirxonabdumurotov@mail.ru>
2022-12-17 22:15:18 +0500

    Fixed return type methods which returns entity

--- Src/MockInterview.Infrastructure/Interface/IGenericRepositoryAsync.cs
+++ Src/MockInterview.Infrastructure/Interface/IGenericRepositoryAsync.cs
@@ -5,11 +5,11 @@ namespace MockInterview.Infrastructure.Interface
 {
     public interface IGenericRepositoryAsync<T> where T : class
     {
-        Task<T> CreateAsync(T entity);
-        Task<T> UpdateAsync(T entity);
-        Task DeleteAsync(Guid id);
+        Task<bool> CreateAsync(T entity);
+        Task<bool> UpdateAsync(T entity);
+        Task<bool> DeleteAsync(Guid id);
         Task<IEnumerable<T>> GetAllAsync();
-        Task<T> Find(Expression<Func<T, bool>> expression);
-        Task<T> Find(Expression<Func<T, bool>> expression, List<string> tables);
+        Task<T> FindAsync(Expression<Func<T, bool>> expression);
+        Task<T> FindAsync(Expression<Func<T, bool>> expression, List<string> tables);
     }
 }
