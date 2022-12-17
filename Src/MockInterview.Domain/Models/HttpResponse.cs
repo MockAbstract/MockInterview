d19ed914b675309b8f6eb7c2fc80e87f374c0430
Nodirkhan Abdumurotov <nodirxonabdumurotov@mail.ru>
2022-12-17 22:49:15 +0500

    Add HttpResponse model

---
+++ Src/MockInterview.Domain/Models/HttpResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace MockInterview.Domain.Models
+{
+    public class HttpResponse<TEntity>
+    {
+        public int TotalCount { get; set; }
+        public IEnumerable<TEntity> Result { get; set; }
+        public bool IsSuccess { get; set; } = true;
+        public int StatusCode { get; set; } = 200;
+        public string StatusMessage { get; set; } = null;
+    }
+}
