28acf5c581c118075caaf375346ff43e39d844d7
Nodirkhan Abdumurotov <nodirxonabdumurotov@mail.ru>
2022-12-17 22:37:20 +0500

    Add service Registration for startup

--- Src/MockInterview.Infrastructure/Extensions/ServiceRegistration.cs
+++ Src/MockInterview.Infrastructure/Extensions/ServiceRegistration.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MockInterview.Infrastructure.Data;
+using MockInterview.Infrastructure.Interface;
+using MockInterview.Infrastructure.Repository;
 
 namespace MockInterview.Infrastructure.Extensions
 {
-    internal class ServiceRegistration
+    public static class ServiceRegistration
     {
+        public static void AddInfrastructureService(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+                configuration.GetConnectionString("DefaultSqlServerConnection")
+                ));
+
+            services.AddScoped<IEmployeeRepositoryAsync, EmployeeRepositoryAsync>();
+
+        }
     }
 }
