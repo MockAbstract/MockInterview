b4d61bbf4bd486c694647bf809001ce742638b58
Nuriddin <isoqov0910@gmail.com>
2022-12-17 21:17:37 +0500

    Add interfase IGenericRepositoryAsync

---
+++ Src/MockInterview.Infrastructure/Interface/IGenericRepositoryAsync.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockInterview.Infrastructure.Interface
+{
+    public interface IGenericRepositoryAsync<T> where T : class
+    {
+        Task<T> CreateAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task DeleteAsync(Guid id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetById(Guid id);
+        Task<T> Find(Func<T, bool> conditionLambda);
+    }
+}
