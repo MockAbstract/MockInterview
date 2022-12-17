f57a16306b16d859ede432f2a0ab38c0dfc819c2
Nodirkhan Abdumurotov <nodirxonabdumurotov@mail.ru>
2022-12-18 03:42:32 +0500

    Adding fields EmployeeUpdate

--- Src/MockInterview.Domain/Models/EmployeeDTO/EmployeeForUpdateDTO.cs
+++ Src/MockInterview.Domain/Models/EmployeeDTO/EmployeeForUpdateDTO.cs
@@ -1,6 +1,19 @@
-﻿namespace MockInterview.Domain.Models.EmployeeDTO
+﻿using System.Text.Json.Serialization;
+
+namespace MockInterview.Domain.Models.EmployeeDTO
 {
-    internal class EmployeeForUpdateDTO
+    public class EmployeeForUpdateDTO : EmployeeDTO
     {
+        [JsonIgnore]
+        public override Guid CreatedBy { get; set; }
+
+        [JsonIgnore]
+        public override Guid UpdatedBy { get; set; }
+
+        [JsonIgnore]
+        public override DateTimeOffset CreatedDate { get; set; }
+
+        [JsonIgnore]
+        public override DateTimeOffset LastModifiedDate { get; set; }
     }
 }
