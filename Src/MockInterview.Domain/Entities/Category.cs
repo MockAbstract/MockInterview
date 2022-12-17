5418a1e872ed0a6f6fa77780ee7671c8b06c9ce5
Nodirkhan Abdumurotov <nodirxonabdumurotov@mail.ru>
2022-12-17 22:38:07 +0500

    Remove unnecessary Id Field

--- Src/MockInterview.Domain/Entities/Category.cs
+++ Src/MockInterview.Domain/Entities/Category.cs
@@ -4,8 +4,6 @@ namespace MockInterview.Domain.Entities
 {
     public class Category : BaseEntity
     {
-        [Required]
-        public Guid Id { get; set; }
 
         [Required]
         public string Name { get; set; }
