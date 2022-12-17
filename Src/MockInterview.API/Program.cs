21cb23ff25dc772ffc0160ef995d21d7afd95bb0
Nuriddin <isoqov0910@gmail.com>
2022-12-18 00:51:35 +0500

    Configur extentions

--- Src/MockInterview.API/Program.cs
+++ Src/MockInterview.API/Program.cs
@@ -1,8 +1,14 @@
+using MockInterview.Business.Extentions;
+using MockInterview.Infrastructure.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+var configuration = builder.Configuration;
 builder.Services.AddControllers();
+builder.Services.AddInfrastructureService(configuration);
+builder.Services.AddBusinessLayer();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
