75f1e8aad8f4e7d14a1e2a8b12146f822491d8b7
Nodirkhan Abdumurotov <nodirxonabdumurotov@mail.ru>
2022-12-18 03:41:09 +0500

    Add Delete Employee Controller

--- Src/MockInterview.API/Controllers/EmployeeController.cs
+++ Src/MockInterview.API/Controllers/EmployeeController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using MockInterview.Business.Interface;
+using MockInterview.Domain.Entities;
+using MockInterview.Domain.Models;
 using MockInterview.Domain.Models.EmployeeDTO;
 
 namespace MockInterview.API.Controllers
@@ -16,11 +18,52 @@ namespace MockInterview.API.Controllers
             this.employeeServiceAsync = employeeServiceAsync;
         }
 
-        [HttpPost]
+        [HttpGet]
+        [ProducesResponseType(typeof(HttpResponse<EmployeeDTO>),200)]
+        public async Task<IActionResult> GetEmployeesAsync()
+        {
+            var response = await employeeServiceAsync.GetAll();
+
+            return StatusCode(response.StatusCode, response);
+        }
+
+        [HttpGet("id")]
+        public async Task<IActionResult> GetEmployeeById(Guid id)
+        {
+            var response = await employeeServiceAsync.GetById(id);
+
+            return StatusCode(response.StatusCode, response);
+        }
+
+        [HttpPost("login")]
         public async Task<IActionResult> LoginAsync(LoginModel model)
         {
             var response = await this.employeeServiceAsync
                 .LoginAsync(model);
+
+            return StatusCode(response.StatusCode, response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync(EmployeeForCreationDTO employee)
+        {
+            var response = await employeeServiceAsync.CreateAsync(employee);
+
+            return StatusCode(response.StatusCode, response);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateAsync(EmployeeForUpdateDTO employee)
+        {
+            var response = await employeeServiceAsync.UpdateAsync(employee);
+
+            return StatusCode(response.StatusCode, response);
+        }
+
+        [HttpDelete("id")]
+        public async Task<IActionResult> DeleteAsync(Guid id)
+        {
+            var response = await employeeServiceAsync.DeleteAsync(id);
 
             return StatusCode(response.StatusCode, response);
         }
