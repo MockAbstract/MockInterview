c97bd5fbb761989ecab8b6540da6a372c4457668
Nuriddin <isoqov0910@gmail.com>
2022-12-17 23:26:34 +0500

    Add Mappinginitializer

--- Src/MockInterview.Business/Helper/MappingInitializer.cs
+++ Src/MockInterview.Business/Helper/MappingInitializer.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MockInterview.Business.Profile
+namespace MockInterview.Business.Helper
 {
     internal class MappingInitializer
     {
--- Src/MockInterview.Business/Services/EmployeeServiceAsync.cs
+++ Src/MockInterview.Business/Services/EmployeeServiceAsync.cs
@@ -1,8 +1,39 @@
 ﻿using MockInterview.Business.Interface;
+using MockInterview.Domain.Models;
+using MockInterview.Domain.Models.EmployeeDTO;
 
 namespace MockInterview.Business.Services
 {
     internal class EmployeeServiceAsync : IEmployeeServiceAsync
     {
+        public Task<HttpResponse<EmployeeDTO>> Create(EmployeeDTO model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpResponse<EmployeeDTO>> Delete(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpResponse<EmployeeDTO>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpResponse<EmployeeDTO>> GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpResponse<EmployeeDTO>> GetPageListAsync(int pageNumber, int pageSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpResponse<EmployeeDTO>> Update(EmployeeDTO model)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
