ddbe91be6d24588c1e45152a9135c22095312fb3
Nodirkhan Abdumurotov <nodirxonabdumurotov@mail.ru>
2022-12-17 22:17:08 +0500

    Create EmployeeRepositoryAsync

---
+++ Src/MockInterview.Infrastructure/Repository/EmployeeRepositoryAsync.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MockInterview.Domain.Entities;
+using MockInterview.Infrastructure.Data;
+using MockInterview.Infrastructure.Interface;
+
+namespace MockInterview.Infrastructure.Repository
+{
+    public class EmployeeRepositoryAsync : GenericRepositoryAsync<Employee>, IEmployeeRepositoryAsync
+    {
+        public readonly DbSet<Employee> Employees;
+        public EmployeeRepositoryAsync(ApplicationDbContext context) : base(context)
+        {
+            this.Employees = context.Employees;
+        }
+
+
+        public async Task<Employee> LoginAsync(string login, string password) => 
+            await FindAsync(e => e.Login == login && e.Password== password);
+    }
+}
