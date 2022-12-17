e0f98c2862f10c3693d298f234b7e4df810112ef
Nuriddin <isoqov0910@gmail.com>
2022-12-17 19:21:43 +0500

    Create category

---
+++ Src/MockInterview.Domain/Entities/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace MockInterview.Domain.Entities
+{
+    public class Category : BaseEntity
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public virtual ICollection<Employee> Specialist { get; set; }
+    }
+}
