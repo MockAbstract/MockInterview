a4beabe6720c97780433146b2a61ed671cc84dba
Nodirkhan Abdumurotov <nodirxonabdumurotov@mail.ru>
2022-12-17 21:15:39 +0500

    Add DefaultEmployee to write Database

---
+++ Src/MockInterview.Infrastructure/Data/SeedData/DefaultEmployee.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MockInterview.Domain.Entities;
+using MockInterview.Domain.Enums;
+
+namespace MockInterview.Infrastructure.Data.SeedData
+{
+    public class DefaultEmployee : IEntityTypeConfiguration<Employee>
+    {
+        public void Configure(EntityTypeBuilder<Employee> builder)
+        {
+            builder.HasData(new Employee
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "Nodirxon",
+                LastName = "Abdumurotov",
+                Role = Role.Admin,
+                CreatedBy = Guid.Empty,
+                CreatedDate = DateTime.Now,
+                Level = Level.None,
+                Experience = TimeSpan.Zero,
+                Login = "admin",
+                Password = "admin"
+            });
+        }
+    }
+}
