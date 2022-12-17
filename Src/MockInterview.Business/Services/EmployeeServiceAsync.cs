9a0179c8f628a292de453f41671867854e36e4aa
Nodirkhan Abdumurotov <nodirxonabdumurotov@mail.ru>
2022-12-18 00:15:07 +0500

    Implemented Login Method

--- Src/MockInterview.Business/Services/EmployeeServiceAsync.cs
+++ Src/MockInterview.Business/Services/EmployeeServiceAsync.cs
@@ -1,14 +1,27 @@
-﻿using MockInterview.Business.Interface;
+﻿using AutoMapper;
+using MockInterview.Business.Interface;
+using MockInterview.Domain.Entities;
 using MockInterview.Domain.Models;
+using MockInterview.Domain.Models.AuthOption;
 using MockInterview.Domain.Models.EmployeeDTO;
+using MockInterview.Infrastructure.Interface;
+using Newtonsoft.Json;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
 namespace MockInterview.Business.Services
 {
     internal class EmployeeServiceAsync : IEmployeeServiceAsync
     {
-        public EmployeeServiceAsync()
-        {
+        private readonly IEmployeeRepositoryAsync employeeRepositoryAsync;
+        private readonly IMapper mapper;
+        private HttpResponse<Employee> response;
 
+        public EmployeeServiceAsync(IEmployeeRepositoryAsync employeeRepositoryAsync, IMapper mapper)
+        {
+            this.employeeRepositoryAsync = employeeRepositoryAsync;
+            this.mapper = mapper;
+            response = new HttpResponse<Employee>();
         }
         public Task<HttpResponse<EmployeeDTO>> Create(EmployeeDTO model)
         {
@@ -35,9 +48,52 @@ namespace MockInterview.Business.Services
             throw new NotImplementedException();
         }
 
+       
+
         public Task<HttpResponse<EmployeeDTO>> Update(EmployeeDTO model)
         {
             throw new NotImplementedException();
         }
+        #region LoginMethod
+        /// <summary>
+        /// Login Method
+        /// </summary>
+        /// <param name="login"></param>
+        /// <returns></returns>
+        public async Task<HttpResponse<string>> LoginAsync(LoginModel login)
+        {
+            HttpResponse<string> Response = new();
+            var employee = await this.employeeRepositoryAsync.LoginAsync(login.Login, login.Password);
+            if (employee is not null)
+            {
+                var claims = GenerateClaims(employee);
+                string token = GetToken(claims);
+                Response.Result = new List<string> { token };
+                return Response;
+            }
+            Response.StatusCode = 404;
+            Response.IsSuccess = false;
+            return Response;
+        }
+        private List<Claim> GenerateClaims(Employee employee)
+        {
+            return new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, employee.Id.ToString()),
+                new Claim(ClaimTypes.Role, employee.Role.ToString()),
+                new Claim("Permissions", JsonConvert.SerializeObject(employee.Permission))
+            };
+        }
+        private string GetToken(List<Claim> claims)
+        {
+            var jwt = new JwtSecurityToken(
+                issuer: AuthOptions.ISSUER,
+                audience: AuthOptions.AUDIENCE,
+                claims: claims,
+                expires: DateTime.UtcNow.AddHours(1)
+                );
+            return new JwtSecurityTokenHandler().WriteToken(jwt);
+        }
+        #endregion
     }
 }
