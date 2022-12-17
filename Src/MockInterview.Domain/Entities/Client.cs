039a0a14096ddf0d7c108aeeac575bd9c52bbc2c
Nodirkhan Abdumurotov <nodirxonabdumurotov@mail.ru>
2022-12-17 18:31:04 +0500

    Create Client Entity

---
+++ Src/MockInterview.Domain/Entities/Client.cs
@@ -0,0 +1,35 @@
+﻿using MockInterview.Domain.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace MockInterview.Domain.Entities
+{
+    public class Client : BaseEntity
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public Level Level { get; set; }
+
+        [Required]
+        public string PhoneNumber { get; set; }
+
+        public TimeSpan Experience { get; set; }
+
+        public string ImagePath { get; set; }
+
+        [Required]
+        public string Login { get; set; }
+        
+        [Required]
+        public string Password { get; set; }
+
+        public DateTime RegisterDate { get; set; } = DateTime.Now;
+
+    }
+}
