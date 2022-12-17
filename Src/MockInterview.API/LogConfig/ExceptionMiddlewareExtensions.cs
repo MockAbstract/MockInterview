48e36794d3be4e7ddedd529d862ceb3c8050f84f
Nodirkhan Abdumurotov <nodirxonabdumurotov@mail.ru>
2022-12-18 02:04:06 +0500

    Add Serilog Configuration

---
+++ Src/MockInterview.API/LogConfig/ExceptionMiddlewareExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using MockInterview.Domain.Models;
+using Serilog;
+using System.Net;
+
+namespace UtilityApi.Logs
+{
+    public static class ExceptionMiddlewareExtensions
+    {
+        public static void ConfigureExceptionHandler(this IApplicationBuilder app)
+        {
+            app.UseExceptionHandler(appError =>
+            {
+                appError.Run(async context =>
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    context.Response.ContentType = "application/json";
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    if (contextFeature != null)
+                    {
+                        Log.Error($"Something went wrong: {contextFeature.Error}");
+                        await context.Response.WriteAsync(new HttpResponse<string>
+                        {
+                            StatusCode = context.Response.StatusCode,
+                            StatusMessage = "Internal Server Error."
+                        }.ToString());
+                    }
+                });
+            });
+        }
+    }
+}
--- Src/MockInterview.API/Program.cs
+++ Src/MockInterview.API/Program.cs
@@ -1,12 +1,20 @@
-var builder = WebApplication.CreateBuilder(args);
+using MockInterview.API.LogConfig;
+using Serilog;
+using UtilityApi.Logs;
 
+var builder = WebApplication.CreateBuilder(args);
+//builder.Host.UseSerilog();
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//var logger = LogConfigurations.SetLogConfiguration(builder)
+  //  .CreateLogger();
 
+//builder.Logging.AddSerilog(logger);
+    
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,6 +23,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+//app.ConfigureExceptionHandler();
 
 app.UseAuthorization();
 
