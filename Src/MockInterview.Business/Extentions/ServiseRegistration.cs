570227381e984343d2cab59ba0ad692fb22dace8
Nuriddin <isoqov0910@gmail.com>
2022-12-17 22:50:47 +0500

    Configure AutoMapper in Busines layer

---
+++ Src/MockInterview.Business/Extentions/ServiseRegistration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MockInterview.Business.Profile;
+
+namespace MockInterview.Business.Extentions
+{
+    public static class ServiseRegistration
+    {
+        public static void AddBusinessLayer(this IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(MappingInitializer));
+
+        }
+    }
+}
