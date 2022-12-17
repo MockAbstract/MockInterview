78664df3713cea92b34b51e5c2badc4015c0452e
Nuriddin <isoqov0910@gmail.com>
2022-12-18 00:19:44 +0500

    Done methods

--- Src/MockInterview.Business/Services/EmployeeServiceAsync.cs
+++ Src/MockInterview.Business/Services/EmployeeServiceAsync.cs
@@ -1,33 +1,65 @@
-﻿using MockInterview.Business.Interface;
+﻿using AutoMapper;
+using MockInterview.Business.Interface;
+using MockInterview.Domain.Entities;
 using MockInterview.Domain.Models;
 using MockInterview.Domain.Models.EmployeeDTO;
+using MockInterview.Infrastructure.Interface;
 
 namespace MockInterview.Business.Services
 {
     internal class EmployeeServiceAsync : IEmployeeServiceAsync
     {
-        public EmployeeServiceAsync()
-        {
+        private readonly IEmployeeRepositoryAsync employeeRepository;
+        private readonly IMapper mapper;
+        private HttpResponse<EmployeeDTO> response;
 
-        }
-        public Task<HttpResponse<EmployeeDTO>> Create(EmployeeDTO model)
+        public EmployeeServiceAsync(IEmployeeRepositoryAsync employeeRepository, IMapper mapper, HttpResponse<EmployeeDTO> response)
         {
-            throw new NotImplementedException();
-        }
-
-        public Task<HttpResponse<EmployeeDTO>> Delete(Guid Id)
-        {
-            throw new NotImplementedException();
+            this.employeeRepository = employeeRepository;
+            this.mapper = mapper;
+            this.response = response;
         }
 
-        public Task<HttpResponse<EmployeeDTO>> GetAll()
+        public virtual async Task<HttpResponse<EmployeeDTO>> Create(EmployeeDTO model)
         {
-            throw new NotImplementedException();
+            var employ = await employeeRepository.FindAsync(employe => employe.Login == model.Login);
+            bool isSuccess = false  ;
+            if (employ.Equals(null))
+            {
+                var employeEntity = mapper.Map<Employee>(model);
+                isSuccess = await employeeRepository.InsertAsync(employeEntity);
+                response.IsSuccess = isSuccess;
+                return response;
+
+            }
+
+            response.IsSuccess = isSuccess;
+            return response;
         }
 
-        public Task<HttpResponse<EmployeeDTO>> GetById(Guid id)
+        public virtual async Task<HttpResponse<EmployeeDTO>> Delete(Guid Id)
         {
-            throw new NotImplementedException();
+            bool isSucces = await employeeRepository.RemoveAsync(Id);
+            response.IsSuccess = isSucces;
+            return response;
+        }
+
+        public virtual async Task<HttpResponse<EmployeeDTO>> GetAll()
+        {
+            var employes =  await employeeRepository.GetAllAsync();
+
+            response.TotalCount = employes.count;
+            response.Result = mapper.Map<IEnumerable<EmployeeDTO>>(employes.entities);
+
+            return response;
+        }
+
+        public async Task<HttpResponse<EmployeeDTO>> GetById(Guid id)
+        {
+            var employe = await employeeRepository.FindAsync(employe => employe.Id.Equals(id));
+            response.Result = mapper.Map<IEnumerable<EmployeeDTO>>(new List<Employee> { employe});
+
+            return response;
         }
 
         public Task<HttpResponse<EmployeeDTO>> GetPageListAsync(int pageNumber, int pageSize)
