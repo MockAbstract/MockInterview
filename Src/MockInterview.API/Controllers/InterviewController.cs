32d845f5c3e4a6320ed475ad9607084aaeadafc9
Nodirkhan Abdumurotov <nodirxonabdumurotov@mail.ru>
2022-12-18 16:46:48 +0500

    Implement Interview Controller

--- Src/MockInterview.API/Controllers/InterviewController.cs
+++ Src/MockInterview.API/Controllers/InterviewController.cs
@@ -1,7 +1,11 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using MockInterview.Business.Interface;
+using MockInterview.Domain.Entities;
 using MockInterview.Domain.Models.InterviewDTO;
+using System.Runtime.CompilerServices;
+using System.Security.Claims;
+using System.Security.Principal;
 
 namespace MockInterview.API.Controllers
 {
@@ -17,7 +21,40 @@ namespace MockInterview.API.Controllers
             this.interviewServiceAsync = interviewServiceAsync;
         }
 
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync([FromBody] InterviewForCreationDTO interview)
+        {
+            ClaimsIdentity identity = (ClaimsIdentity)User.Identity;
+            var userId = Guid
+                .Parse(identity.FindFirst(ClaimTypes.NameIdentifier).Value);
 
+            var response = await interviewServiceAsync.CreateAsync(interview, userId);
 
+            return StatusCode(response.StatusCode, response);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateAsync([FromBody] InterviewForModifiedDTO interview)
+        {
+            ClaimsIdentity identity = (ClaimsIdentity)User.Identity;
+            var userId = Guid
+                .Parse(identity.FindFirst(ClaimTypes.NameIdentifier).Value);
+
+            var response = await interviewServiceAsync.UpdateAsync(interview, userId);
+
+            return StatusCode(response.StatusCode, response);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteAsync(Guid Id)
+        {
+            ClaimsIdentity identity = (ClaimsIdentity)User.Identity;
+            var userId = Guid
+                .Parse(identity.FindFirst(ClaimTypes.NameIdentifier).Value);
+
+            var response = await interviewServiceAsync.DeleteAsync(Id, userId);
+
+            return StatusCode(response.StatusCode, response);
+        }
     }
 }
