6b1bd5d5191752d8ace0ca9c9e3312c846184065
Nuriddin <isoqov0910@gmail.com>
2022-12-18 16:21:05 +0500

    Add InterviewController

---
+++ Src/MockInterview.API/Controllers/InterviewController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MockInterview.Business.Interface;
+using MockInterview.Domain.Models.InterviewDTO;
+
+namespace MockInterview.API.Controllers
+{
+    [Route("interview-management/interviews")]
+    [ApiController]
+    [Authorize]
+    public class InterviewController : ControllerBase
+    {
+        private readonly IInterviewServiceAsync interviewServiceAsync;
+
+        public InterviewController(IInterviewServiceAsync interviewServiceAsync)
+        {
+            this.interviewServiceAsync = interviewServiceAsync;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(HttpResponse<InterviewDTO>), 200)]
+        public virtual async Task<IActionResult> GetInterview()
+
+    }
+}
