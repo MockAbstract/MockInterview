058869cdc1c31c966b4b06747f9e68a4c959f141
Nuriddin <isoqov0910@gmail.com>
2022-12-18 11:29:11 +0500

    Add CategoryController

---
+++ Src/MockInterview.API/Controllers/CategoryController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MockInterview.Business.Interface;
+using MockInterview.Domain.Models;
+using MockInterview.Domain.Models.CategoryDTO;
+
+namespace MockInterview.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryServiceAsync categoryServiceAsync;
+
+        public CategoryController(ICategoryServiceAsync categoryServiceAsync)
+        {
+            this.categoryServiceAsync = categoryServiceAsync;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(HttpResponse<CategoryDTO>), 200)]
+        public virtual async Task<IActionResult> GetCategoryAsync()
+        {
+            var response = await categoryServiceAsync.GetAllAsync();
+
+            return StatusCode(response.StatusCode, response);
+        }
+    }
+}
