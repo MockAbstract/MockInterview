ec27ce556e8484f0edc919f62bae8be359b0f291
Nuriddin <isoqov0910@gmail.com>
2022-12-18 11:30:21 +0500

    Add EmployeeForGetDTO

---
+++ Src/MockInterview.Domain/Models/EmployeeDTO/EmployeeForGetDTO.cs
@@ -0,0 +1,32 @@
+﻿using MockInterview.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockInterview.Domain.Models.EmployeeDTO
+{
+    public class EmployeeForGetDTO
+    {
+        [Required]
+        public virtual Guid Id { get; set; }
+
+        [Required]
+        public virtual string FirstName { get; set; }
+
+        [Required]
+        public virtual string LastName { get; set; }
+
+        [Required]
+        public virtual Level Level { get; set; }
+
+        [Required]
+        public virtual string PhoneNumber { get; set; }
+
+        public virtual string ImagePath { get; set; }
+        public virtual DateTimeOffset ExperienceStartDate { get; set; }
+        public virtual DateTimeOffset ExperienceEndDate { get; set; }
+    }
+}
